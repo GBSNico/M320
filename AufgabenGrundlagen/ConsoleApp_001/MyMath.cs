836b82b7db9bf07c7acd28b809cf848714bcd983
GBSNico <nico.reisacher@edu.gbssg.ch>
2025-08-13 16:05:56 +0200

    init2

---
+++ AufgabenGrundlagen/ConsoleApp_001/MyMath.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_001
+{
+    internal class MyMath
+    {
+        public static int Calc_ggT(int a, int b)
+        {
+            int z = 0;
+
+            while (b != 0)
+            {
+                z = a % b;
+
+                a = b;
+
+                b = z;
+            }
+
+            return a;
+        }
+
+        public static int Calc_kgV(int a, int b)
+        {
+            return ((a * b) / Calc_ggT((int)a, (int)b));
+        }
+
+        public static float Average(int[] values)
+        {
+            return (float)values.Average();
+        }
+
+        public static int Max(int[] values)
+        {
+            return values.Max();
+        }
+
+        public static int Min(int[] values)
+        {
+            return values.Min();
+        }
+
+        public static int ReadInt()
+        {
+            int number = 0;
+
+            while (number == 0 || int.IsNegative(number))
+            {
+                Console.WriteLine("Bitte gib eine Zahl die grösser als 0 ist ein:");
+
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+
+            return (int)number;
+        }
+    }
+}
---
+++ AufgabenGrundlagen/ConsoleApp_001/Program.cs
@@ -0,0 +1,76 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string RepeatLoop = "y";
+
+            while (RepeatLoop != "n" && RepeatLoop != "N")
+            {
+                Console.WriteLine("Was möchtest du berechnen? 'ggT'/'kgV'/'mittel'");
+                string UserInput = Console.ReadLine();
+
+                switch (UserInput)
+                {
+                    case "ggt":
+                    case "ggT":
+                    case "gGt":
+                    case "gGT":
+                    case "Ggt":
+                    case "GgT":
+                    case "GGt":
+                    case "GGT":
+                        {
+                            int a = MyMath.ReadInt();
+                            int b = MyMath.ReadInt();
+
+                            Console.WriteLine($"Der ggT von {a} und {b} ist {MyMath.Calc_ggT(a, b)}");
+                        }
+
+                        break;
+
+                    case "kgv":
+                    case "kgV":
+                    case "kGv":
+                    case "kGV":
+                    case "Kgv":
+                    case "KgV":
+                    case "KGv":
+                    case "KGV":
+                        {
+                            int a = MyMath.ReadInt();
+                            int b = MyMath.ReadInt();
+
+                            Console.WriteLine($"Das kgV von {a} und {b} ist {MyMath.Calc_kgV(a, b)}");
+                        }
+
+                        break;
+
+                    case "mittel":
+                    case "m":
+                    case "Mittel":
+                    case "M":
+                        {
+                            int[] UserNumbers = [];
+                            int? CurrentUserNumber = null;
+
+                            while (CurrentUserNumber != 0)
+                            {
+                                Console.WriteLine("Bitte gib eine Zahl ein: (Abbrechen mit '0')");
+
+                                CurrentUserNumber = Convert.ToInt32(Console.ReadLine());
+                            }
+                        }
+
+                        break;
+                }
+
+                Console.WriteLine("Möchtest du eine weitere Berechnung machen? 'y'/'n'");
+                RepeatLoop = Console.ReadLine();
+            }
+        }
+    }
+}
